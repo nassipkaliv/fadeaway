0033e5f2061f8cef68037338bc03ae70876ad9df
nassipkaliv <nassipkaliv@gmail.com>
2023-06-01 22:58:36 +0500

    some edits

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import RouterRoutes from './routes/RouterRoutes';
 import Footer from './components/common/Footer';
 import BackTop from './components/common/BackTop';
 import { FiltersProvider } from './contexts/filters/filtersContext';
+import React from 'react';
 
 
 const App = () => {
